564d32503118ab02e42fd763389230d245dc2575
youyiwan <youyiwan@gmail.com>
2024-01-28 18:01:03 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Euan";
+
+        System.out.println("Hello! I'm " + name+"\n"+"What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+
+}
+
+
