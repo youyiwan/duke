9bb7b6f969434b957f9ae688ef98ac07b77d5f4a
youyiwan <youyiwan@gmail.com>
2024-01-29 20:34:05 +0800

    Echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
 
         String name = "Euan";
 
         System.out.println("Hello! I'm " + name+"\n"+"What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true)
+        {
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye"))
+            {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else System.out.println(line);
+        }
+
+
 
     }
 
